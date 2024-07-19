e475d3d8f21eb3ec555c4fa587b23b145b80d5f6
mariaivanovaa <maria21.1vanowa@gmail.com>
2024-07-19 17:22:17 +0300

    added changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ for (let i = 1; i <= 10; i++) {
 
 // 4
 const array = [1, 2, 3, 4, 5]
- let i = 0;
+ let a = 0;
 while (i < array.length) {
     console.log(array[i])
     i++
@@ -31,23 +31,23 @@ while (i < array.length) {
 // 5
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === 7) {
+for (let b = 0; i < numbers.length; i++) {
+    if (numbers[b] === 7) {
         break;
     }
-    console.log(numbers[i]);
+    console.log(numbers[b]);
 }
 
 // 6
 const number = 10
-let i = 1
+let c = 1
 while (i <= number) {
     console.log(i)
     i++
 }
 
 // 7
-let i = 0
+let d = 0
 while (i < 20) {
     i++
     
